3f19f28840b64e39143b70c89dc2654a0e3c337e
ClaytonOSouza <clayton.os2005@gmail.com>
2020-04-29 12:44:28 -0300

    deploy

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,8 @@
-provider "aws" {
-  region     = "us-east-1"  
-  access_key = "AKIAUY7ODRBKY5IBJHDF"
-  secret_key = "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"
-}
+provider "aws" {}
 
 resource "aws_instance" "devopsaws" {
   ami = "ami-07d0cf3af28718ef8"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   subnet_id = "subnet-59f1d777"
   key_name = "devopsawsclayton"
   tenancy = "default"
